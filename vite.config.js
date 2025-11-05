03b35f5fbb5655ea1cf3a03916899d9369926f73
naeem123r <naeemilyas2140@gmail.com>
2025-11-05 21:44:06 +0500

    aa

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
+    port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5001',
         changeOrigin: true,
       },
     },

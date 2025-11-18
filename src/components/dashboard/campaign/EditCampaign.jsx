b32b7585333857bd3d299ea6a56d515709409be7
Anas Noor <anasnoor099@gmail.com>
2025-11-18 21:20:59 +0500

    conflicts solve

--- src/components/dashboard/campaign/EditCampaign.jsx
+++ src/components/dashboard/campaign/EditCampaign.jsx
@@ -1,6 +1,4 @@
 
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from '../../../utills/privateIntercept';
